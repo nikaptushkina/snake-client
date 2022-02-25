14e20773d8a47d80edb91645c700e17b45da0de5
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-02-25 12:32:13 -0800

    create play.js, connect with server, handle incoming data and log it to player

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// connection with game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // handles incoming data and console.logs it to the player
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
