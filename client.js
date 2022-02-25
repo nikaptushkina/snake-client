cdda08e6bdce2840d0113acddff0cda734115f22
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-02-25 13:11:45 -0800

    create connect event, print initials upon connection

--- client.js
+++ client.js
@@ -8,11 +8,21 @@ const connect = function () {
   });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding('utf8');
+
+  // event triggered upon connection
+  conn.on('connect', (connect) => {
+    console.log('Successfully connected to game server');
+  });
 
   // handles incoming data and console.logs it to the player
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  //prints name to server
+  conn.on('connect', () => {
+    conn.write('Name: NIK');
   });
 
   return conn;
